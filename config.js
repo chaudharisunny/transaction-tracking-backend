6d66dd3ad76c03d378f0d491f880a62e2283a19c
chaudhari sunny <chaudharisunny6@gmail.com>
2025-12-05 17:11:51 +0530

    update code

--- config.js
+++ config.js
@@ -1,18 +1,13 @@
-// db/connectdb.js
-
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URL, {
-      
-    });
-    console.log(" Database connected");
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("Database connected");
   } catch (error) {
-    console.error(" Database connection failed:", error.message);
+    console.error("Database connection failed:", error.message);
     process.exit(1);
   }
 };
 
 export default connectDB;
-//mongodb+srv://chaudharisunny6:jPHJipwsdGkdLnn2@cluster0.z9jqfaf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
